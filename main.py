f41b107234e1489ab6a38a9cdd7c8b2dc18b7203
ASUKARU Shouhei <askaru2014@gmail.com>
2018-10-09 10:34:04 +0900

    NewDesign

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # -* encoding utf-8 *-
 #  python 3.6.2
 from flask import Flask, render_template, url_for, Response
+import socket
+import csv
+import datetime
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
@@ -16,9 +19,13 @@ def add_header(response):
 
 @app.route('/')
 def index():
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    with open('logs/user_logs.csv', 'a', encoding='utf-8') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow([datetime.datetime.now(), host, ip])
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
