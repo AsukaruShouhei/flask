203fddb617a01e0241fd6eb92758a21960b54c34
ASUKARU Shouhei <askaru2014@gmail.com>
2019-01-24 20:24:57 +0900

    main

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # -* encoding utf-8 *-
 #  python 3.6.2
-from flask import Flask, render_template, url_for, Response, request, redirect
+from flask import Flask, render_template, url_for, Response, request
 import socket
 import csv
 import datetime
-import os
-import Forms.contactFrom as conForm
-from flask_mail import Mail
-from flask_mail import Message
+import pyautogui
+import time
+import smtplib
+from email.mime.text import MIMEText
 
 
 app = Flask(__name__)
-mail = Mail(app)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
+
 
 
 @app.after_request
@@ -27,37 +27,30 @@ def add_header(response):
 def index():
     host = socket.gethostname()
     ip = socket.gethostbyname(host)
-    savepath = './user_logs/'
-    form = conForm.MyForm()
-    if not os.path.exists(savepath):
-        os.mkdir(savepath)
-        os.chmod(savepath, 777)
-    with open(savepath + 'user_logs.csv', 'a', encoding='utf-8') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow([datetime.datetime.now(), host, ip, request.referrer])
-    if request.referrer in '/contact':
-        success = 'メッセージを送信しました。現在内容を精査しておりますので今しばらくお待ちください。'
-    else:
-        success = None
-    return render_template('index.html', form=form, success=success)
+    # with open('logs/user_logs.csv', 'a', encoding='utf-8') as f:
+    #     writer = csv.writer(f, lineterminator='\n')
+    #     writer.writerow([datetime.datetime.now(), host, ip])
+    return render_template('index.html')
 
 
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
     if request.method == 'POST':
-        email = request.form['email']
-        message = request.form['message']
-        msg = Message(message,
-                      sender=email,
-                      recipients=["asukaru2014@icloud.com"])
-        # mail.send(msg)
-        host = socket.gethostname()
-        ip = socket.gethostbyname(host)
-        savepath = './user_logs/'
-        with open(savepath + 'mail.csv', 'a', encoding='utf-8') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerow([datetime.datetime.now(), host, ip, request.referrer, email, message])
-        return redirect('/')
+        success = 'お問合せありがとうございます。'
+        body = request.form['message']
+        msg = MIMEText(body)
+        msg["Subject"] = "お客様からお問い合わせ"
+        msg["From"] = "ASUKARU WEB サイトからのお問い合わせ"
+        msg["To"] = "asukaru.endo@gmail.com"
+        s = smtplib.SMTP("smtp.gmail.com", 587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login("asukaru.endo@gmail.com", "as2014es")
+        s.send_message(msg)
+        s.close()
+
+        return render_template('index.html', success=success)
 
 
 if __name__ == '__main__':
